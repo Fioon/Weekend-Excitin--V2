a5559e00c3dc3c0da91cd7b2c7d256a1d5bd60fb
Fioon <135500399+Fioon@users.noreply.github.com>
2024-07-27 14:43:21 +0800

    Update ControlsSubState.hx

--- source/options/ControlsSubState.hx
+++ source/options/ControlsSubState.hx
@@ -110,6 +110,9 @@ class ControlsSubState extends MusicBeatSubstate {
 			}
 		}
 		changeSelection();
+			#if android
+		addVirtualPad(FULL, A_B);
+		#end
 	}
 
 	var leaving:Bool = false;
@@ -128,7 +131,12 @@ class ControlsSubState extends MusicBeatSubstate {
 
 			if (controls.BACK) {
 				ClientPrefs.reloadControls();
+				#if android
+				FlxTransitionableState.skipNextTransOut = true;
+				FlxG.resetState();
+				#else
 				close();
+				#end
 				FlxG.sound.play(Paths.sound('cancelMenu'));
 			}
 
